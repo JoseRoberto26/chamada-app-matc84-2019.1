e736ecf47f249f7d677d05cf660ccfc7206bc45f
JR-PC\JR <jr.lsf26@gmail.com>
2019-05-24 00:26:29 -0300

    Front e Back - base

---
+++ chamada-app-back/chamada-app-back/src/main/kotlin/com/matc841/equipe5/chamadaappback/ChamadaAppBackApplication.kt
@@ -0,0 +1,11 @@
+package com.matc841.equipe5.chamadaappback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChamadaAppBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChamadaAppBackApplication>(*args)
+}
---
+++ chamada-app-back/chamada-app-back/src/test/kotlin/com/matc841/equipe5/chamadaappback/ChamadaAppBackApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.matc841.equipe5.chamadaappback
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ChamadaAppBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

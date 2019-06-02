a9a0a7ceaee2e50153fff4248f5088e4bf845378
Lucas Souza <alfsouza708@gmail.com>
2019-06-01 22:43:00 -0300

    Adiciona tela login

--- chamada-app-front/src/Components/Login/login.js
+++ chamada-app-front/src/Components/Login/login.js
@@ -2,17 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {
     efetuarLogin
-} from '../../Redux/Actions/LoginAction'
+} from '../../Redux/Actions/LoginAction';
+import '../../bootstrap.css'
+import './login.css'
+import logo from './logo.svg'
+import {Link} from 'react-router-dom'
 
 export class Login extends React.Component {
 
     render() {
        
         return(
-            <div>
-                <button type="button" onClick={() => this.props.efetuarLogin()} >Click Me!</button>
-                <span>{this.props.jojo}</span>
-            </div>
+            <>
+                <div class="header">
+                    <img src={logo} alt="PresençaFácil" />
+                </div>
+
+                <div class="pagina-com-formulario">
+                    <div class="form">
+                        <form class="login-form">
+                        <input type="text" placeholder="CPF"/>
+                        <input type="password" placeholder="Senha"/>
+                        <button onclick="Login()">entrar</button>
+                        <p class="message">Não está registrado? <Link to={"/"}>Cadastre-se aqui</Link></p>
+                        </form>
+                    </div>
+                </div>
+            </>
         )
     }
     
